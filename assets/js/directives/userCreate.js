b6bf1551f9d704bdcf7595f5c4f557d400934b93
Mike Rensel <mike.rensel@mktec.com>
2015-08-22 18:56:07 -0400

    create user with validations

---
+++ assets/js/directives/userCreate.js
@@ -0,0 +1,27 @@
+angular.module('appModule').directive('userCreate',['UserModel', '$state', '$stateParams', '$timeout', function(UserModel, $state, $stateParams, $timeout) {
+  return {
+    restrict: 'A',
+    templateUrl: 'templates/directives/userCreate.html',
+    link: function (scope, element, attrs) {
+      var timer;
+      scope.user = {};
+      scope.showError = false;
+      scope.createUser = function() {
+        console.log(scope);
+        $timeout.cancel(timer);
+        scope.showError = false;
+        scope.form.$submitted = true;
+        if(scope.form.$invalid) {
+          scope.showError = true;
+          timer = $timeout(function() {
+            scope.showError = false;
+          }, 3000);
+        }else {
+          UserModel.create(scope.user).then(function() {
+            $state.go('main.users');
+          });
+        }
+      };
+    }
+  }
+}]);
