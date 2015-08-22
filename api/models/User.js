d05f07f5d7133fdc0b819edd73975537891aab78
Mike Rensel <mike.rensel@mktec.com>
2015-08-22 16:39:47 -0400

    user model

--- api/models/User.js
+++ api/models/User.js
@@ -8,7 +8,30 @@
 module.exports = {
 
   attributes: {
-
+    id: {
+      type: 'integer',
+      size: 15,
+      primaryKey: true
+    },
+    firstName: {
+      type: 'string',
+      size: 25,
+      required: true
+    },
+    lastName: {
+      type: 'string',
+      size: 25,
+      required: true
+    },
+    gitHubId: {
+      type: 'string',
+      size: 50,
+      required: true
+    },
+    email: {
+      type: 'string',
+      size: 100,
+      required: true
+    }
   }
 };
-
