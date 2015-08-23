49cbce8dc3d1e23a760bafa3607baff6da539a29
Mike Rensel <mike.rensel@mktec.com>
2015-08-22 20:07:08 -0400

    added not found message to details page

--- assets/js/directives/userDisplaySelected.js
+++ assets/js/directives/userDisplaySelected.js
@@ -5,11 +5,14 @@ angular.module('appModule').directive('userDisplaySelected',['UserModel', 'Githu
     templateUrl: 'templates/directives/userDisplaySelected.html',
     link: function (scope, element, attrs) {
       scope.user = {};
+      scope.gitNotFound = false;
       UserModel.find($stateParams.id).then(function(data) {
         scope.user = data;
         GithubModel.findRepos(data.gitHubId).then(function(data) {
           console.log(data);
           scope.repos = data;
+        }, function(err) {
+          scope.gitNotFound = true;
         });
       });
       scope.editUser = function() {
